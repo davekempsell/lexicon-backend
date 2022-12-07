40b4649b5a961c0c5f69ec5e94fd1994f573500d
davekempsell <dave_kempsell@hotmail.com>
2022-12-07 10:51:50 +0000

    port change

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', (req,res) => {
   })
 })
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3030;
 
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`)
